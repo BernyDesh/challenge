82cbaab878e946bd06fe198eb691313eb172443c
bernardita <berny.aguayo.tejos@gmail.com>
2021-10-18 01:52:06 -0300

    cong sonar

---
+++ app/sonar-project.js
@@ -0,0 +1,18 @@
+const sonarqubeScanner = require("sonarqube-scanner");
+sonarqubeScanner(
+  {
+    serverUrl: process.env.SONARQUBE_HOST || "http://localhost:9000",
+    // token: "",
+    options: {
+      "sonar.projectName": "ChallengeSonar",
+      "sonar.projectVersion": "0.0.1",
+      "sonar.projectDescription": "SonarQube for Challenge",
+      "sonar.projectKey": "ChallengeSonar:Test",
+      "sonar.sources":
+        "./app.js, ./config/db.config.js, ./routes/developers.js",
+        "sonar.javascript.lcov.reportPaths": "./coverage/lcov.info",
+      // "sonar.inclusions": "./", // Entry point of your code
+    },
+  },
+  () => {}
+);
